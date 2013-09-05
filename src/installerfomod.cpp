47ba8271aabe1b1fcbb0f7a94f64b643e61f084b
Tannin <devnull@localhost>
2013-09-05 21:14:08 +0200

    updated versions

--- src/installerfomod.cpp
+++ src/installerfomod.cpp
@@ -38,7 +38,7 @@ QString InstallerFomod::description() const
 
 VersionInfo InstallerFomod::version() const
 {
-  return VersionInfo(1, 2, 1, VersionInfo::RELEASE_FINAL);
+  return VersionInfo(1, 2, 2, VersionInfo::RELEASE_FINAL);
 }
 
 bool InstallerFomod::isActive() const
