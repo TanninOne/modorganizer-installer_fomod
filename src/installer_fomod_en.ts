06637e645aab6b942d2c92547f670b55d75e3625
Tannin <sherb@gmx.net>
2016-06-04 20:36:54 +0200

    translation

---
+++ src/installer_fomod_en.ts
@@ -0,0 +1,174 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>FomodInstallerDialog</name>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="14"/>
+        <source>FOMOD Installer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="25"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="55"/>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="62"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="76"/>
+        <source>Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="83"/>
+        <source>&lt;a href=&quot;#&quot;&gt;Link&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="183"/>
+        <source>Manual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="193"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="200"/>
+        <location filename="fomodinstallerdialog.cpp" line="1428"/>
+        <location filename="fomodinstallerdialog.cpp" line="1568"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.ui" line="207"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="205"/>
+        <location filename="fomodinstallerdialog.cpp" line="249"/>
+        <source>Failed to parse ModuleConfig.xml. See console for details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="216"/>
+        <source>ModuleConfig.xml missing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="469"/>
+        <source>invalid plugin state %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="645"/>
+        <source>&lt;a href=&quot;%1&quot;&gt;Link&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="667"/>
+        <source>unsupported order type %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="685"/>
+        <source>unsupported group type %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="945"/>
+        <source>All components in this group are required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="986"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="1015"/>
+        <source>Select one or more of these options:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="1400"/>
+        <source>Disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="1402"/>
+        <source>This button is disabled because the following group(s) need a selection: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="1428"/>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="1488"/>
+        <source>This component is required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="1494"/>
+        <source>It is recommended you enable this component</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="1500"/>
+        <source>Optional component</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="1505"/>
+        <source>This component is not usable in combination with other installed plugins</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fomodinstallerdialog.cpp" line="1513"/>
+        <source>You may be experiencing instability in combination with other installed plugins</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallerFomod</name>
+    <message>
+        <location filename="installerfomod.cpp" line="44"/>
+        <source>Installer for xml based fomod archives.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installerfomod.cpp" line="241"/>
+        <source>Installation as fomod failed: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installerfomod.cpp" line="265"/>
+        <source>image formats not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installerfomod.cpp" line="267"/>
+        <location filename="installerfomod.cpp" line="278"/>
+        <source>invalid problem key %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installerfomod.cpp" line="275"/>
+        <source>This indicates that files from dlls/imageformats are missing from your MO installation or outdated. Images in installers may not be displayed. Please re-install MO</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
